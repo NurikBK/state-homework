1f58a3d8d5ebe44a78311c3eca5e0148fe63c7d8
Nurzhan Bizhan <bizhannk@gmail.com>
2023-04-19 18:56:10 +0600

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css';
+import Article from './components/Article';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import JoinUs from './components/JoinUs';
+import LearnMore from './components/LearnMore';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Hero />
+        <Article />
+        <LearnMore />
+        <JoinUs />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
