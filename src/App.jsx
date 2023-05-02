ae4ce1f0f4deaf4bfc9d642054e11d5762195490
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-02 18:32:50 +0600

    fix path

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Home from './components/Home';
-
-import { Routes, Route } from 'react-router-dom';
-import NotFoundPage from './components/NotFoundPage';
-import UserPage from './components/UserPage';
-import CommunityPage from './components/CommunityPage';
+import CommunityPage from './pages/CommunityPage';
+import UserPage from './pages/UserPage';
+import NotFoundPage from './pages/NotFoundPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/community" element={<CommunityPage />} />
         <Route path="/community/:userId" element={<UserPage />} />
         <Route path="/community/*" element={<NotFoundPage />} />
